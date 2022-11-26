be0c0251dda017bf2f53041501d7fdb6f6fcb973
Rodrigo <rod.gomez.o@gmail.com>
2022-11-26 14:42:11 +0100

    Anadir comentarios.

--- src/repositorios/LibreriaSingleton.java
+++ src/repositorios/LibreriaSingleton.java
@@ -6,25 +6,50 @@ import java.util.Random;
 
 import modelos.Libro;
 
+/**
+ * Patron sigleton para almacenar todos los libros.
+ * Esta clase permite almacenar todos los libros en memoria que estan disponibles para la aplicacion.
+ * 
+ * @author Rodrigo
+ *
+ */
 public class LibreriaSingleton {
 	
+	// Unica instancia disponible de la libreria
 	private static LibreriaSingleton libreria;
 
+	// Listado de libros
 	private List<Libro> libros;
 	
+	/**
+	 * Constructor privado para que no pueda ser instanciado por ninguna otra clase.
+	 */
 	private LibreriaSingleton() {
-		
-		libros = new ArrayList<Libro>();
-		for (int i=0;i<50;i++) {
-			Libro libro = new Libro();
-			libro.setIsbn("ISBN:_" + i);
-			libro.setNombre("Nombre: " + i);
-			libro.setReservado(new Random().nextBoolean());
-			libros.add(libro);
-		}
-		
+		libros = cargarLibros();
 	}
 	
+	/**
+	 * Metodo para cargar los libros.
+	 * NOTE: Este metodo se ha cargado con libros de ejemplo para que puedan ser visualizados en las distintas ventas de la aplciacion.
+	 * TODO: Adaptar el metodo para que pueda leer los libros del disco duro.
+	 * @return
+	 */
+	private List<Libro> cargarLibros() {
+		List<Libro> data = new ArrayList();
+		for (int i=0;i<50;i++) {
+			Libro libro = new Libro();
+			libro.setIsbn("ISBN:_" + libro.getId());
+			libro.setNombre("Nombre: " + libro.getId());
+			libro.setReservado(new Random().nextBoolean());
+			data.add(libro);
+		}
+		return data;
+	}
+
+	/**
+	 * Metodo para obtener la libreria, en caso de que no se haya creado aun llamara al constructor y esta a su vez se encargara de cargar los libros.
+	 * @return
+	 */
 	public static LibreriaSingleton getLibreria() {
 		if (libreria==null) {
 			libreria = new LibreriaSingleton();
@@ -32,6 +57,10 @@ public class LibreriaSingleton {
 		return libreria;
 	}
 
+	/**
+	 * Se devuelven los libros
+	 * @return
+	 */
 	public List<Libro> getLibros() {
 		return libros;
 	}
