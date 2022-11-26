1821d2da3380bd144d1d9df0a28f8df50f116721
Rodrigo <rod.gomez.o@gmail.com>
2022-11-26 17:26:51 +0100

    Cambiar vista al pulsar el boton de detalles.

--- src/controladores/MainController.java
+++ src/controladores/MainController.java
@@ -15,6 +15,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import modelos.Libro;
+import repositorios.LibreriaSingleton;
 
 public class MainController {
 
@@ -31,19 +33,43 @@ public class MainController {
 	void verDetallesLibro(ActionEvent event) {
 		try {
 			System.out.println("Ver detalles libro");
-			// Mostrar vista ver detalles libro
-			FXMLLoader loaderDetallesLibro = new FXMLLoader(getClass().getResource("/vistas/DetalleLibro.fxml"));
-			DetalleLibroController detallesLibroController = new DetalleLibroController();
-			loaderDetallesLibro.setController(detallesLibroController);
-			Parent root = loaderDetallesLibro.load();
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root));
-			stage.show();
+			mostrarVistaListaLibro();
+//			mostrarVistaDetallesLibro();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+	
+	private void mostrarVistaListaLibro() throws IOException{
+		// Mostrar vista lista libro
+		FXMLLoader loaderListaLibros = new FXMLLoader(getClass().getResource("/vistas/LibrosList.fxml"));
+		LibrosListController listaLibroController = new LibrosListController(LibreriaSingleton.getLibreria().getLibros());
+		loaderListaLibros.setController(listaLibroController);
+		Parent root = loaderListaLibros.load();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.setTitle(LibrosListController.NOMBRE_VENTANA);
+		stage.setMinHeight(LibrosListController.MIN_HEIGHT);
+		stage.setMinWidth(LibrosListController.MIN_WIDTH);
+		stage.show();
+	}
+
+	private void mostrarVistaDetallesLibro() throws IOException {
+		// Mostrar vista ver detalles libro
+		FXMLLoader loaderDetallesLibro = new FXMLLoader(getClass().getResource("/vistas/DetalleLibro.fxml"));
+		DetalleLibroController detallesLibroController = new DetalleLibroController();
+		Libro libro = new Libro();
+		libro.setIsbn("ABC");
+		libro.setNombre("Titulo Libro");
+		libro.setReservado(false);
+		loaderDetallesLibro.setController(detallesLibroController);
+		detallesLibroController.setLibro(libro);
+		Parent root = loaderDetallesLibro.load();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.show();
 	}
 
 	@FXML // This method is called by the FXMLLoader when initialization is complete
