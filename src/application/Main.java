4ad87c3ae5d3d0d3b7e0dec1ac82cf572f002ac7
Rodrigo <rod.gomez.o@gmail.com>
2023-01-13 11:00:17 +0100

    Almacenar ficheros cuando se cierre la aplicacion.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,46 @@
 package application;
 	
+
+import java.util.List;
+
+import config.AppConfiguration;
 import controladores.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import modelos.Usuario;
 import repositorios.BibliotecariosSingleton;
+import repositorios.LibreriaSingleton;
+import repositorios.ReservaSingleton;
 import repositorios.UsuariosSingleton;
 
 
 public class Main extends Application {
 
 	@Override
+	public void stop() throws Exception {
+		System.out.println("Cerrando la aplicacion");
+		almacenarFicheros();
+		System.out.println("Ficheros almacenados.");
+		super.stop();
+	}
+
+	private void almacenarFicheros() {
+		System.out.println("Almacenar archivos de configuracion de la aplicacion");
+		AppConfiguration.getConfiguration().almacenarAjustes();
+		BibliotecariosSingleton.getRepoUsuarios().escribirUsuarios();
+		LibreriaSingleton.getLibreria().escribirLibros();
+		ReservaSingleton.getReservaSingleton().escribirReservas();
+		UsuariosSingleton.getRepoUsuarios().escribirUsuarios();
+		System.out.println("-----------------------------------------------------");
+	}
+
+	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BibliotecariosSingleton repoBibliotecarios = BibliotecariosSingleton.getRepoUsuarios();
 			UsuariosSingleton repoUsuarios = UsuariosSingleton.getRepoUsuarios();
-			LoginController.mostrarLogin(primaryStage, repoBibliotecarios.getUsuarios(), repoUsuarios.getUsuarios());
+			List<Usuario> usuarios = repoUsuarios.getUsuarios();
+			LoginController.mostrarLogin(primaryStage, repoBibliotecarios.getUsuarios(), usuarios);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
